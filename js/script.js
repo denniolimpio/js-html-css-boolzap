f8fa7fd80517e17bc94c5798120a84247ea369b7
dennis <dennis.olimpio0794@gmail.com>
2020-06-17 15:59:05 +0200

    ho aggiunto setTimeout per mostrare un messaggio di risposta dopo alcuni secondi.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 $(document).ready( function(){
-
-
 // quando clicco sull'icona dell'aereplano stampo il contenuto inserito all'interno della input in ms_message-view.
 //-- per stampare quanto inserito nella input ho bisogno di "leggere" il suo contenuto con val().
 
 $(".btn-send").click( function () {
 
-inviaMessaggio()
+inviaMessaggio();
+
+setTimeout(receivedMessage, 3000);
 
 });
 
 });
-
-
 
  function inviaMessaggio () {
 
@@ -35,7 +33,7 @@ inviaMessaggio()
      var timeMessage = hours + " : " + minutes;
 
    //stampo l'orario
-   
+
    $(".templates .ms_messageTime span").text(timeMessage);
 
 
@@ -49,15 +47,41 @@ inviaMessaggio()
 
        $(cloneTemplate).addClass("send");
 
-
-
-
-
        // resetto il  contenuto della input
 
-      // testoMessaggio = $(".ms_form-message input").val("");
+      testoMessaggio = $(".ms_form-message input").val("");
 
    }
 
-
 };
+
+function receivedMessage () {
+
+  // leggo il contenuto inserito nella input
+       $(".templates .ms_messageText span").text(" Ciao Dennis! come va?");
+
+
+       // inserisco l'orario di invio del messaggio
+
+       var date = new Date ();
+       var hours = date.getHours();
+       var minutes = date.getMinutes();
+       var timeMessage = hours + " : " + minutes;
+
+     //stampo l'orario
+
+     $(".templates .ms_messageTime span").text(timeMessage);
+
+
+       // clono il template in cui inserire il contenuto della input
+         var cloneTemplate = $(".templates .ms_message").clone();
+
+        // aggiungo in templates in ms_message-view
+         $(".ms_message-view").append(cloneTemplate);
+
+         // aggiungo la classe send
+
+         $(cloneTemplate).addClass("received");
+
+
+     };
