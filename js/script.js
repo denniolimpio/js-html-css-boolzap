cdf1bb8cd76640a84131f84a2754383e3d30dac0
dennis <dennis.olimpio0794@gmail.com>
2020-06-19 17:17:25 +0200

    Consegna finale
    add possibilità di passare da una chat all'altra. Add commenti.

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $(document).ready( function(){
 
     if((event.which === 13) || (event.keyCode === 13)){
       inviaMessaggio();
-      setTimeout(receivedMessage, 3000);
+      setTimeout(receivedMessage, 1000);
     }
   });
 
@@ -30,7 +30,7 @@ $(document).ready( function(){
 
     var searchInput = $(this).val();
 
-    var contactList = $(".ms_chat-item");
+    var contactList = $(".ms_contact-item");
 
     contactList.each( function(){
 
@@ -52,22 +52,61 @@ $(document).ready( function(){
 
   // il menu a tendina una volta cliccato resta visibile.
   // si chiude quando :
-  // --->  apro un altro menu a tendina;
-  // ---> se clicco nuovamente sul icona "chevron"
+  // --->  a) apro un altro menu a tendina;
+  // ---> b) se clicco nuovamente sul icona "chevron"
 
-  $(document).on( "click", ".ms_with-dropdown", function () {
+  $(document).on( "click", ".ms_arrow", function () {
 
-    $(this).children(".ms_dropdown").toggleClass("active");
+//  a)
+    $(this).parents().siblings().find(".ms_option").removeClass("active");
+    // è possibile farlo anche not().
 
+//  b)
+    $(this).children(".ms_option").toggleClass("active");
   });
 });
 
+
 // Aperto il menu a tendina, se clicco su "elimina messaggio" questo viene cancellato.
+
 $(document).on( "click", ".ms_delete", function () {
 
   $(this).parents(".ms_message").remove();
 
 })
+
+
+// ---- lista Chat
+//  clicco sul contatto e genero un evento:
+$(document).on( "click", ".ms_contact-item", function () {
+
+  // ---> rimuovo la classe selected a tutti gli elementi (di default mostro Michele)
+$(".ms_contact-item").removeClass("selected");
+
+//---> aggiungo la classe selected all'elemento cliccato
+$(this).addClass("selected");
+
+//  mostro solo la chat del contatto con classe selected
+// Quindi
+// --> rimuovo a tutte le chat la classe "show-chat"
+$(".chat").removeClass("active");
+// ---> leggo il valore di data-contact
+var contactData = $(this).attr("data-contact");
+
+// ---> cerco l'elemento chat che ha l'attributo data-chat === data-contact
+// -->  aggingo la classe "show-chat"
+$('.chat[data-chat="' +  contactData +'"]').addClass("active");
+
+// devo modificare l'immagine e il testo dell'elemen nella top-bar right
+
+// ---> prendo il nome e l'immagine del contatto selezionato
+//
+var contactName = $(this).find(".ms_name").text();
+var contactAvatar = $(this).find("img").attr("src");
+
+$(".current-chat").find("h3").text(contactName);
+$(".current-chat").find("img").attr("src", contactAvatar);
+});
 
 // fine document.ready
 
@@ -83,7 +122,7 @@ $(document).on( "click", ".ms_delete", function () {
 // inserisco l'ora all'interno del template;
 // ---> $(".templates .ms_messageTime span");
 // rendo visibile il templates  all'interno della area di visualizzazione dei messaggi;
-// ---> ms_message-view;
+// ---> ms_messageContainer;
 // ---> aggiungo a ms_message la classe send (con gli stili css);
 // resetto il  contenuto della input;
 // Con scrollTop vado automati. alla fine della pagina per mostare l'ultimo messaggio inviato;
@@ -106,12 +145,12 @@ function inviaMessaggio () {
 
     var cloneTemplate = $(".templates .ms_message").clone();
 
-    $(".ms_message-view").append(cloneTemplate);
+    $(".ms_messageContainer").append(cloneTemplate);
 
     $(cloneTemplate).addClass("send");
     testoMessaggio = $(".ms_form-message input").val("");
 
-    $(".ms_message-view").scrollTop($(".ms_message-view").height());
+    $(".ms_messageContainer").scrollTop($(".ms_messageContainer").height());
 
   }
 
@@ -121,7 +160,7 @@ function inviaMessaggio () {
 // scrivo il testo da stampare;
 // genero l'ora di ricezione del messaggio:
 // clono il template e lo inserisco all'interno -->
-// ---> ms_message-view;
+// ---> ms_messageContainer;
 // ---> aggiungo a ms_message la classe received (con gli stili css);
 // Con scrollTop vado automati. alla fine della pagina per mostare l'ultimo messaggio ricevuto;
 
@@ -137,10 +176,10 @@ function receivedMessage () {
   $(".templates .ms_messageTime span").text(timeMessage);
 
   var cloneTemplate = $(".templates .ms_message").clone();
-  $(".ms_message-view").append(cloneTemplate);
+  $(".ms_messageContainer").append(cloneTemplate);
 
   $(cloneTemplate).addClass("received");
-  $(".ms_message-view").scrollTop($(".ms_message-view").height());
+  $(".ms_messageContainer").scrollTop($(".ms_messageContainer").height());
 };
 
 
